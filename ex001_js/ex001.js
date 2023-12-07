88139887c9970337c528df6f68c56bf728ed0432
edwilsonjunior <edwilsonpessoal@gmail.com>
2023-12-07 13:16:21 -0300

    criei o exercicio

--- ex001_js/ex001.js
+++ ex001_js/ex001.js
@@ -1,13 +1,18 @@
 /*Se listarmos todos os números naturais abaixo 10 que são múltiplos de 3 ou 5, Nós temos 3,5,6 e 9. A soma desses múltiplos é 23 Encontre a soma de todos os múltiplos de  3 ou 5 abaixo de 1000. */
-
-
-
 //contador que vai de 1 a 1000.
-for (var i = 1; i <= 1000; i++) {
-    console.log(i);
-    // more statements
-  }
 //definir o que é um número que é multiplo de 3 ou 5
 //se for multiplo some
 //somar todos os números que são multiplos de 3 ou 5
 //dar o resultado
+
+
+
+
+let soma = 0
+for (let i = 1; i <= 1000; i++) {
+    if(i % 3 === 0  || i % 5 === 0 ){
+      soma += i
+    }
+  }
+  
+  console.log(soma)
